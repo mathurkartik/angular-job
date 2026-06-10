838fed1783905925a47a694c47c2eae9494513be
Kartik Mathur <kartik.mathur@cubastion.com>
2026-06-10 17:50:05 +0530

    fix: update broken Wipro careers URL

--- config/companies_service.py
+++ config/companies_service.py
@@ -9,7 +9,7 @@ CATEGORY_LABEL = "Category 3: Service-Based (Market Intel)"
 COMPANIES = [
     {"name": "TCS",           "tier": "Tier_8_Background_Service", "url": "https://www.tcs.com/careers/india",            "portal_type": "generic"},
     {"name": "Infosys",       "tier": "Tier_8_Background_Service", "url": "https://www.infosys.com/careers/",             "portal_type": "generic"},
-    {"name": "Wipro",         "tier": "Tier_8_Background_Service", "url": "https://careers.wipro.com/careers-home/jobs?keywords=Engineer",                   "portal_type": "generic"},
+    {"name": "Wipro",         "tier": "Tier_8_Background_Service", "url": "https://careers.wipro.com/search/?q=Engineer",                   "portal_type": "generic"},
     {"name": "HCLTech",       "tier": "Tier_8_Background_Service", "url": "https://www.hcltech.com/careers",              "portal_type": "generic"},
     {"name": "Cognizant",     "tier": "Tier_8_Background_Service", "url": "https://careers.cognizant.com/global/en/search-results?keywords=Engineer",      "portal_type": "generic"},
     {"name": "Accenture",     "tier": "Tier_8_Background_Service", "url": "https://www.accenture.com/in-en/careers/jobsearch?keyword=Engineer",      "portal_type": "generic"},
